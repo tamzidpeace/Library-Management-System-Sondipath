22d8c0b5459f8657e8471849452418458ee0bd20
tamzidpeace <tamjedpeace@gmail.com>
2020-09-03 18:08:04 +0600

    fixing some fonted

--- resources/views/admin/book/info.blade.php
+++ resources/views/admin/book/info.blade.php
@@ -2,24 +2,42 @@
 
 @section('content')
 
-    <div class="box box-solid box-default" style="background: ; margin-left:40px;border:10px;">
-        <h2>Details of {{ $book->name }}</h1>
+    <div class="row-fluid">
 
-            <br>
-            @if ($book->cover)
-                <img src="/image/cover/{{ $book->cover }}" class="img-thumbnail" style="height: 200px; width:170px;">
-            @else
-                <img src="/image/cover/book2.jpg" class="img-thumbnail" style="height:  200px; width:170px;">
-            @endif
+        <div style="margin-left: 30px; margin-bottom: 40px;">
+            <h2 class="box-title">Details of {{ $book->name }}</h2>
+        </div>
 
-            <br> <br>
+        <div class="span2">
+
+            <div class="box box-success">
+
+                <div class="box-body">
+
+                    <div style="margin-left: 0px">
+                        @if ($book->cover)
+                            <img src="/image/cover/{{ $book->cover }}"  style="height: 200px; width:170px;">
+                        @else
+                            <img src="/image/cover/book2.jpg" class="img-thumbnail" style="height:  200px; width:170px;">
+                        @endif
+                    </div>
+
+                </div><!-- /.box-body -->
+            </div>
+
+        </div>
+
+        <div class="span4">
             <p><strong>Authors:</strong> {{ $book->authors }}</p>
             <p><strong>ISBN:</strong> {{ $book->isbn }}</p>
-            <p><strong>Purchase Price:</strong> {{ $book->pprice }}</p>
-            <p><strong>Selling Price:</strong> {{ $book->sprice }}</p>
+            <p><strong>Purchase Price:</strong> {{ $book->purchase_price }}</p>
+            <p><strong>Selling Price:</strong> {{ $book->selling_price }}</p>
             <p><strong>Copyright:</strong> {{ $book->copyright }}</p>
             <p><strong>Year:</strong> {{ $book->year }}</p>
             <p><strong>Country:</strong> {{ $book->country }}</p>
+        </div>
+
+        <div class="span4">
             <p><strong>Category:</strong> {{ $book->category }}</p>
             <p><strong>Publisher:</strong> {{ $book->publisher }}</p>
             <p><strong>Language:</strong> {{ $book->language }}</p>
@@ -27,8 +45,11 @@
             <p><strong>Edition:</strong> {{ $book->edition }}</p>
             <p><strong>First Entry:</strong> {{ $book->created_at }}</p>
             <p><strong>Last Update:</strong> {{ $book->updated_at }}</p>
+        </div>
+
 
     </div>
+
 
 
 @endsection
